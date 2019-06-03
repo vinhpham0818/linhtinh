afb4f8c0ed6223810a2c2e5a1cf78fa25bd6d78e
vinhpham0818 <vinhhvbd@gmail.com>
2019-06-03 15:10:50 +0700

    reactjs

---
+++ src/pages/contact/contact.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './contact.css'
+
+class ContactPage extends React.Component {
+    render () {
+        return (
+            <React.Fragment>
+                <div className="main-contact">
+                    Hello
+                </div>
+                <div>
+                    lkl;lkj;l
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default ContactPage
---
+++ src/pages/products/products.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class ProductsPage extends React.Component {
+    render () {
+        return (
+            
+                <div>
+                    Hello
+                </div>
+                
+        )
+    }
+}
+
+export default ProductsPage
